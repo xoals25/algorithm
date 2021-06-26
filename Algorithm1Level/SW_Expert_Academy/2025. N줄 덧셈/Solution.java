7d9dcc59833e733d0e1922fb49465c9e7fa956ee
xoals25 <xoals25@naver.com>
2021-06-27 01:58:55 +0900

    2021.06.27 d1 난이도 - SW Expert Academy

---
+++ Algorithm1Level/SW_Expert_Academy/2025. N줄 덧셈/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/**
+ * 다른 사람 코드 보니 수학 계산방식으로 간단하게 만들 수 있다는걸 깨닳았다.
+ * */
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int A;
+        int B;
+        A=sc.nextInt();
+        B=sc.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+    }
+}
