a220bfb37eaa85f6ea9f198976709dd844724fb4
xoals25 <xoals25@naver.com>
2021-06-27 02:02:45 +0900

    2021.06.27 d1 난이도 - SW Expert Academy #.다주 간단한 계산기와 N줄 덧셈 바뀐거 변경해줌

--- Algorithm1Level/SW_Expert_Academy/2025. N줄 덧셈/Solution.java
+++ Algorithm1Level/SW_Expert_Academy/2025. N줄 덧셈/Solution.java
@@ -5,19 +5,34 @@ import java.io.FileInputStream;
  * 다른 사람 코드 보니 수학 계산방식으로 간단하게 만들 수 있다는걸 깨닳았다.
  * */
 
+/*내 코드*/
 class Solution
 {
     public static void main(String args[]) throws Exception
     {
-        Scanner sc = new Scanner(System.in);
-        int A;
-        int B;
-        A=sc.nextInt();
-        B=sc.nextInt();
 
-        System.out.println(A+B);
-        System.out.println(A-B);
-        System.out.println(A*B);
-        System.out.println(A/B);
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        int result = 0;
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            result = result+test_case;
+
+        }
+        System.out.println(result);
     }
 }
+
+/*다른 사람 코드 */
+class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        System.out.println(((1+T)*T)/2);
+    }
+}
+
+/**
+ * 다른 사람 코드 보니 수학 계산방식으로 간단하게 만들 수 있다는걸 깨닳았다.
+ * */
