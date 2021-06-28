543838da5491e053dfebb5706ada09845406b3f4
xoals25 <xoals25@naver.com>
2021-06-29 04:49:39 +0900

    2021.06.29 d1 난이도 - SW Expert Academy

--- Algorithm1Level/SW_Expert_Academy/2050. 알파벳을 숫자로 변환/Solution.java
+++ Algorithm1Level/SW_Expert_Academy/2050. 알파벳을 숫자로 변환/Solution.java
@@ -2,50 +2,39 @@ import java.util.Scanner;
 
 /**
  * 풀이 과정
- * 1. 유효성 검사 하기 위한 정보 나열
- * 2. 주어진 문자열을 요효성 검사하기 위해 잘라 놓기
- * 3. 유효성 검사후 출력
+ * 1.주어진 알파벳으로 이루어진 문자열을 한문자 한문자 씩 나눈다.
+ * 2.한문자씩 숫자로 변환(아스키코드로 변환)
+ * 3.변환된 아스키코드에서 원하는 답에 맞게 계산해주고 출력
  * */
 
 /**
  * 새로 알게된 내장 함수
- * .substring(0,4); //0부터 4전까지 자르기 즉, 0123 (문자의 인덱스)이 나온다.
- * .sutstring(6); //6번의 인덱수부터 끝까지 자른다.
+ * .toCharArray();
+ * 문자열을 각각 문자로 나눠 문자 배열(char[])로 넣어준다.
+ *
+ * char[] charList = A.toCharArray();
  * */
 
+/**
+ * 알면 좋을것 같은 사실들
+ *  char[] input =  A.trim().toCharArray();
+ *  trim() : 문자열의 앞,뒤 공백 제거
+ *  toCharArray() : 문장을 새로운 문자 배열로 변환
+ * */
 
-
-
-import java.util.Scanner;
-
+/**
+ * 같은 방식으로 Stirng[]을 만들어줘서 A.split("")해줘서 해줘도 괜찮을것같다.
+ * */
 
 class Solution
 {
     public static void main(String args[]) throws Exception
     {
-
         Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
-        sc.nextLine();
-        int[] monthDay = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-        for(int i=1; i<=T; i++){
-            String date = sc.nextLine();
-
-            String year = date.substring(0,4); //0부터 4전까지 자르기 즉, 0123이 나온다.
-            String month =date.substring(4,6);
-            String day =date.substring(6);
-
-            int monthToInt = Integer.parseInt(month);
-            int dayToInt = Integer.parseInt(day);
-            if(0<monthToInt && monthToInt <13 && 0<dayToInt && dayToInt <=monthDay[monthToInt-1]){
-                System.out.println("#"+i+" "+year+"/"+month+"/"+day);
-            }
-            else{
-                System.out.println("#"+i+" -1");
-            }
+        String A = sc.nextLine();
+        char[] charList = A.toCharArray();
+        for(int i=0; i<charList.length; i++){
+            System.out.print((int) charList[i]-64 +" ");
         }
-
     }
 }
